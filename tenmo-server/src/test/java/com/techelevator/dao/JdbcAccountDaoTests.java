42d41ece9d965bab9787a6e63b7011635d503d20
Zachary Anderson <anderson.zac@gmail.com>
2022-07-06 16:17:59 -0400

    testing, tweaks to jdbcaccountdao

--- tenmo-server/src/test/java/com/techelevator/dao/JdbcAccountDaoTests.java
+++ tenmo-server/src/test/java/com/techelevator/dao/JdbcAccountDaoTests.java
@@ -1,5 +1,7 @@
 package com.techelevator.dao;
 
+import com.techelevator.tenmo.dao.JdbcAccountDao;
+import com.techelevator.tenmo.model.Account;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,6 +11,24 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import java.util.List;
 
-
 public class JdbcAccountDaoTests extends BaseDaoTests{
+
+    //need to create some test Accounts
+    private JdbcTemplate jdbcTemplate;
+    private JdbcAccountDao sut;
+
+    @Before
+    public void setup() {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+        sut = new JdbcAccountDao(jdbcTemplate);
+    }
+
+    @Test
+    public void list_accounts_given_user_id() {
+        List<Account> foundAccount= sut.findAllAccounts();
+
+        String sql = ""
+
+
+    }
 }
