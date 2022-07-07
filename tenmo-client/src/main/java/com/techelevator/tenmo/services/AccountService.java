a8c38b8c969d33b49de287a9a5f6a931cb9ccd7a
Zachary Anderson <anderson.zac@gmail.com>
2022-07-07 11:34:46 -0400

    accountservice client side minor updates, need to use authorized user

--- tenmo-client/src/main/java/com/techelevator/tenmo/services/AccountService.java
+++ tenmo-client/src/main/java/com/techelevator/tenmo/services/AccountService.java
@@ -1,9 +1,25 @@
 package com.techelevator.tenmo.services;
 
+import com.techelevator.tenmo.model.Account;
+import com.techelevator.util.BasicLogger;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
 
 public class AccountService {
 
     private String baseUrl = "http://localhost:8080/";
     private RestTemplate restTemplate = new RestTemplate();
+
+    public Account getAccount() {
+        Account myaccount = null;
+        try {
+            myaccount = restTemplate.getForObject(baseUrl + "account/", Account.class);
+        } catch (RestClientResponseException | ResourceAccessException e) {
+            BasicLogger.log(e.getMessage());
+        }
+        return myaccount;
+
+    }
+
 }
